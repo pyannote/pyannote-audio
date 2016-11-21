9d50090f5b36317d43af9b56a9238967c3a911a8
Hervé BREDIN <bredin@limsi.fr>
2016-11-21 14:26:22 +0100

    fix: add missing "import"

--- pyannote/audio/embedding/glue.py
+++ pyannote/audio/embedding/glue.py
@@ -32,6 +32,7 @@ import multiprocessing
 from pyannote.audio.embedding.losses import unitary_cosine_triplet_loss
 from pyannote.audio.embedding.losses import unitary_euclidean_triplet_loss
 from pyannote.audio.embedding.losses import unitary_angular_triplet_loss
+from pyannote.audio.embedding.generators import DerivativeBatchGenerator
 
 
 class Glue(object):
