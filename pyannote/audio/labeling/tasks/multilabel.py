1d7e79750d2259944ce0fff31b0a9031a8360ad1
MarvinLvn <marvin.lavechin@ensimag.grenoble-inp.fr>
2019-07-19 16:14:40 -0400

    Huge refactoring of tasks/multilabel.py

--- pyannote/audio/labeling/tasks/multilabel.py
+++ pyannote/audio/labeling/tasks/multilabel.py
@@ -34,13 +34,21 @@ FEM     (female speech)
 MAL     (male speech)
 """
 
-import torch
+from itertools import cycle
+
 import numpy as np
+import torch
+from pyannote.audio.features import Precomputed
+from pyannote.core import Annotation
+from pyannote.core import SlidingWindowFeature
+from pyannote.core import Timeline
+from pyannote.core.utils.numpy import one_hot_encoding
+from pyannote.database import get_annotated
+from pyannote.database import get_protocol
+
 from .base import LabelingTask
 from .base import LabelingTaskGenerator
-from .base import TASK_MULTI_LABEL_CLASSIFICATION
-from pyannote.database import get_protocol
-from pyannote.audio.features import Precomputed
+from .. import TASK_MULTI_LABEL_CLASSIFICATION
 
 
 class MultilabelGenerator(LabelingTaskGenerator):
@@ -92,55 +100,103 @@ class MultilabelGenerator(LabelingTaskGenerator):
     >>>     pass
     """
 
-    def __init__(self, feature_extraction, protocol, subset='train',
-                 frame_info=None, frame_crop=None, duration=3.2,
-                 batch_size=32, per_epoch=1, parallel=1,
-                 overlap=False, speech=False, labels=None, shuffle=True):
-        self.overlap = overlap
-        self.speech = speech
-        self.labels_ = labels
+    def __init__(self, feature_extraction, protocol, labels,
+                 subset='train', frame_info=None, frame_crop=None,
+                 duration=3.2, batch_size=32, per_epoch=1, parallel=1,
+                 shuffle=True):
+
+        self.labels_spec = labels
         super().__init__(feature_extraction, protocol, subset=subset,
                          frame_info=frame_info, frame_crop=frame_crop,
                          duration=duration,
                          batch_size=batch_size, per_epoch=per_epoch,
                          parallel=parallel, shuffle=shuffle)
 
-    def postprocess_y(self, Y):
-        # number of speakers for each frame
-        speaker_count = np.sum(Y, axis=1, keepdims=True)
+    @staticmethod
+    def derives_label(annotation, derivation_type, meta_label, regular_labels):
+        """
+        Derives a label. The derivation takes as inputs :
+        - An annotation from which we want to derive
+        - A derivation type : union or intersection
+        - A meta label : the name of the output label
+        - A list of regular labels : the regular labels from which we want to derive
 
-        # mark speech regions as such
-        speech = np.int64(speaker_count > 0)
+        Example :
+            1) derives_label(annotation, 'union', 'speech', ["CHI","MAL","FEM"]
+            Will compute the speech label based on the union of "CHI", "MAL" and "FEM"
+            2) derives_label(annotation, 'intersection', 'overlap', ["CHI","MAL","FEM"]
+            Will compute the overlapping speech based on the intersection of "CHI", "MAL" and "FEM"
 
-        # Last column is speech if BabyTrain (only where UU* speakers are speaking)
-        # Needs to be removed
-        if "SPEECH" in self.labels_ and Y.shape[1] == len(self.labels_):
-            Y = Y[:, :-1]
+        annotation:  Annotation type
+            The annotation we want to derive from.
+        derivation_type: string, must belong to ['union', 'intersection']
+            The derivation type
+        meta_label:  string
+            The meta label, the name of the label returned by the derivation
+        regular_labels:  list of strings
+            A list of regular labels we want to derive from
+        """
+        if derivation_type not in ['union', 'intersection']:
+            raise ValueError("Derivation type must be in ['union', 'intersection')")
 
-        # We add overlap first (columns are alphabetically sorted by their CLASS id)
-        if self.overlap:
-            # mark overlap regions as such
-            overlap = np.int64(speaker_count > 1)
-            Y = np.concatenate((Y, overlap), axis=1)
-        # Then we can add speech
-        if self.speech:
-            Y = np.concatenate((Y, speech), axis=1)
+        derived = Annotation()
+        renaming = {k: v for k, v in zip(regular_labels, [meta_label] * len(regular_labels))}
+        annotation = annotation.subset(regular_labels).rename_labels(mapping=renaming)
 
-        # replace NaNs by 0s
-        Y = np.nan_to_num(Y)
-        return Y
+        if derivation_type == 'union':
+            support = annotation.support()
+            derived.update(support.rename_tracks())
+        elif derivation_type == 'intersection':
+            overlap = Timeline()
+            for track1, track2 in annotation.co_iter(annotation):
+                if track1 == track2:
+                    continue
+                overlap.add(track1[0] & track2[0])
+            derived = overlap.support().to_annotation(generator=cycle([meta_label]))
+
+        return derived
+
+    def initialize_y(self, current_file):
+        # First, one hot encode the regular classes
+        annotation = current_file['annotation'].subset(self.labels_spec['regular'])
+        y, _ = one_hot_encoding(annotation,
+                                get_annotated(current_file),
+                                self.frame_info,
+                                labels=self.labels_spec["regular"],
+                                mode='center')
+        y_data = y.data
+        # Then, one hot encode the meta classes
+        for derivation_type in ['union', 'intersection']:
+            for meta_label, regular_labels in self.labels_spec[derivation_type].items():
+                derived = self.derives_label(current_file["annotation"], derivation_type, meta_label, regular_labels)
+                z, _ = one_hot_encoding(derived, get_annotated(current_file),
+                                        self.frame_info,
+                                        labels=[meta_label],
+                                        mode='center')
+
+                y_data = np.hstack((y_data, z.data))
+
+        return SlidingWindowFeature(self.postprocess_y(y_data),
+                                    y.sliding_window)
 
     @property
     def specifications(self):
         return {
             'task': TASK_MULTI_LABEL_CLASSIFICATION,
             'X': {'dimension': self.feature_extraction.dimension},
-            'y': {'classes': self.labels_},
+            'y': {'classes': self.labels_spec["regular"]
+                             + list(self.labels_spec['union'].keys())
+                             + list(self.labels_spec['intersection'].keys())},
         }
 
 
 class Multilabel(LabelingTask):
-    """Train a 4-labels classifier
+    """
+    Train a n-labels classifier where the labels are provided by the user, and can be of 3 types :
+
+    - Regular labels : those are computed directly from the annotation and are kept unchanged.
+    - Union meta-label : those are computed by taking the union of multiple regular labels
+    - Intersection meta-label : those are computed by taking the intersection of multiple regular labels.
 
     Parameters
     ----------
@@ -155,6 +211,19 @@ class Multilabel(LabelingTask):
         Number of prefetching background generators. Defaults to 1.
         Each generator will prefetch enough batches to cover a whole epoch.
         Set `parallel` to 0 to not use background generators.
+    labels:
+        regular: list
+            List of classes that need to be predicted.
+        union:
+            Dictionnary of union meta-labels whose keys are the meta-label names,
+            and values are a list of regular classes
+        intersection:
+            Dictionnary of intersection meta-labels whose keys are the meta-label names,
+            and values are a list of regular classes
+    weighted_loss: bool, optional, default to False
+        Compute weights that will be send later to the model.
+        For each of the regular classes : 1/prior
+        For each of the union/intersection classes : 1
 
     Usage
     -----
@@ -170,38 +239,30 @@ class Multilabel(LabelingTask):
 
     # evaluation protocol
     >>> from pyannote.database import get_protocol
-    >>> protocol = get_protocol('BabyTrain.SpeakerRole.JSALT')
+    >>> protocol = get_protocol('BabyTrain.SpeakerDiarization.All')
 
     # train model using protocol training set
     >>> for epoch, model in task.fit_iter(model, precomputed, protocol):
     ...     pass
-
     """
-    def __init__(self, protocol_name, preprocessors, weighted_loss=False, overlap=False, speech=False, **kwargs):
+    def __init__(self, protocol_name, preprocessors, labels, weighted_loss=False, **kwargs):
         super(Multilabel, self).__init__(**kwargs)
+
         # Need protocol to know the classes that need to be predicted
         # And thus the dimension of the target !
         self.weighted_loss = weighted_loss
-        self.overlap = overlap
-        self.speech = speech
+
+        # Labels related attributes
+        self.labels_spec = labels
+        self.label_names = labels["regular"] \
+                           + list(labels['union'].keys()) \
+                           + list(labels['intersection'].keys())
+        self.nb_labels = len(labels)
+        self.nb_regular_labels = len(labels["regular"])
+
+        # Protocol, so that we can loop through the training set
+        # and compute the prior
         self.protocol = get_protocol(protocol_name, preprocessors=preprocessors)
-        self.labels_ = self._update_labels()
-
-    def _update_labels(self):
-        """
-        Get the actual number of labels (the roles amongst {CHI,FEM,KCHI,MAL,SPEECH,OVERLAP} that are present
-        in the data
-        """
-        labels = set()
-        for current_file in getattr(self.protocol, "train")():
-            y_labels = set(current_file["annotation"].labels())
-            labels |= y_labels
-
-        if self.overlap:
-            labels.add("OVERLAP")
-        if self.speech:
-            labels.add("SPEECH")
-        return sorted(labels)
 
     def get_batch_generator(self, feature_extraction, protocol, subset='train',
                             frame_info=None, frame_crop=None):
@@ -214,18 +275,15 @@ class Multilabel(LabelingTask):
             per_epoch=self.per_epoch,
             batch_size=self.batch_size,
             parallel=self.parallel,
-            overlap=self.overlap,
-            speech=self.speech,
-            labels=self.labels_)
+            labels=self.labels_spec)
 
     def _get_one_over_the_prior(self):
-        nb_classes = len(self.labels_)
-        weights = dict([(key, 0.0) for key in self.labels[0:nb_classes]])
+        weights = {k: 0.0 for k in self.label_names[0:self.nb_regular_labels]}
 
         # Compute the cumulated duration
         for current_file in self.protocol.train():
             y = current_file["annotation"]
-            for speaker in self.labels[0:nb_classes]:
+            for speaker in self.label_names[0:self.nb_regular_labels]:
                 weights[speaker] += y.label_duration(speaker)
 
         total_speech = sum(weights.values(), 0.0)
@@ -234,11 +292,13 @@ class Multilabel(LabelingTask):
                 weights[key] = total_speech/value
 
         # Finally normalize, so that the weights sum to 1
-        # We remove speaker roles whose value is equal to 0
         norm1 = sum(weights.values())
-        weights = {key: value/norm1 for key, value in weights.items() if value != 0}
+        regular_weights = {key: value/norm1 for key, value in weights.items()}
+        meta_weights = {key: 1 for key in list(self.labels_spec["union"].keys())
+                        + list(self.labels_spec["intersection"].keys())}
 
-        return torch.tensor(np.array(list(weights.values())), dtype=torch.float32)
+        weights = list(regular_weights.values()) + list(meta_weights.values())
+        return torch.tensor(np.array(weights), dtype=torch.float32)
 
     @property
     def weight(self):
