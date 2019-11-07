585d2db9a9171f12502a1194626ec41661e02242
paul lerner <lerner@m135>
2019-11-07 15:23:21 +0100

    feat: allows to optimise the diarization pipeline using coverage with.. fixed purity. TODO make both class cohabit

--- pyannote/audio/pipeline/speaker_diarization.py
+++ pyannote/audio/pipeline/speaker_diarization.py
@@ -31,7 +31,7 @@ from pathlib import Path
 from pyannote.core import Annotation
 from pyannote.database import get_annotated
 
-from pyannote.metrics.diarization import GreedyDiarizationErrorRate
+from pyannote.metrics.diarization import GreedyDiarizationErrorRate,DiarizationPurityCoverageFMeasure
 
 from .speech_turn_segmentation import SpeechTurnSegmentation
 from .speech_turn_clustering import SpeechTurnClustering
@@ -42,8 +42,7 @@ from typing import Union
 from pyannote.pipeline import Pipeline
 from pyannote.pipeline.parameter import Uniform
 
-
-class SpeakerDiarization(Pipeline):
+class DiarizationDER(Pipeline):
     """Speaker diarization pipeline
 
     Parameters
@@ -61,7 +60,6 @@ class SpeakerDiarization(Pipeline):
         Clustering method. Defaults to 'pool'.
     evaluation_only : `bool`
         Only process the evaluated regions. Default to False.
-
     Hyper-parameters
     ----------------
     min_duration : `float`
@@ -160,10 +158,188 @@ class SpeakerDiarization(Pipeline):
             shrt_speech_turns, copy=False).support(collar=0.)
 
         # TODO. add GMM-based resegmentation
+    # def loss(self, current_file: dict, hypothesis: Annotation) -> float:
+    #     """Compute (1 - coverage) at target purity
+    #
+    #     If purity < target, return 1 + (1 - purity)
+    #
+    #     Parameters
+    #     ----------
+    #     current_file : `dict`
+    #         File as provided by a pyannote.database protocol.
+    #     hypothesis : `pyannote.core.Annotation`
+    #         Speech turns.
+    #
+    #     Returns
+    #     -------
+    #     error : `float`
+    #         1. - cluster coverage.
+    #     """
+    #     metric = DiarizationPurityCoverageFMeasure()
+    #     reference  = current_file['annotation']
+    #     uem = get_annotated(current_file)
+    #     f_measure = metric(reference, hypothesis, uem=uem)
+    #     purity, coverage, _ = metric.compute_metrics()
+    #     if purity > self.purity:
+    #         return 1. - coverage
+    #     else:
+    #         return 1. + (1. - purity)
 
+    #TODO : find out a way for loss and get_metric to cohabit
     def get_metric(self) -> GreedyDiarizationErrorRate:
-        """Return new instance of diarization error rate metric"""
-        return  GreedyDiarizationErrorRate(collar=0.0, skip_overlap=False)
+       """Return new instance of diarization error rate metric"""
+       return  GreedyDiarizationErrorRate(collar=0.0, skip_overlap=False)
+
+class SpeakerDiarization(Pipeline):
+    """Speaker diarization pipeline
+
+    Parameters
+    ----------
+    sad_scores : `Path` or 'oracle'
+        Path to precomputed speech activity detection scores.
+        Use 'oracle' to assume perfect speech activity detection.
+    scd_scores : `Path`
+        Path to precomputed SCD scores on disk
+    embedding : `Path`
+        Path to precomputed embedding on disk
+    metric : {'euclidean', 'cosine', 'angular'}, optional
+        Metric used for comparing embeddings. Defaults to 'cosine'.
+    method : {'pool', 'affinity_propagation'}
+        Clustering method. Defaults to 'pool'.
+    evaluation_only : `bool`
+        Only process the evaluated regions. Default to False.
+    purity : `float`, optional
+        Target purity. Defaults to 0.9
+    Hyper-parameters
+    ----------------
+    min_duration : `float`
+        Do not cluster speech turns shorter than `min_duration`. Assign them to
+        the closest cluster (of long speech turns) instead.
+    """
+
+    def __init__(self, sad_scores: Optional[Union[Path, str]] = None,
+                       scd_scores: Optional[Path] = None,
+                       embedding: Optional[Path] = None,
+                       metric: Optional[str] = 'cosine',
+                       method: Optional[str] = 'pool',
+                       evaluation_only: Optional[bool] = False,
+                       purity: Optional[float] = 0.9):
+
+        super().__init__()
+        self.sad_scores = sad_scores
+        self.scd_scores = scd_scores
+        self.speech_turn_segmentation = SpeechTurnSegmentation(
+            sad_scores=self.sad_scores,
+            scd_scores=self.scd_scores)
+        self.evaluation_only = evaluation_only
+        self.purity = purity
+
+        self.min_duration = Uniform(0, 10)
+
+        self.embedding = embedding
+        self.metric = metric
+        self.method = method
+        self.speech_turn_clustering = SpeechTurnClustering(
+            embedding=self.embedding, metric=self.metric, method=self.method)
+
+        self.speech_turn_assignment = SpeechTurnClosestAssignment(
+            embedding=self.embedding, metric=self.metric)
+
+    def __call__(self, current_file: dict) -> Annotation:
+        """Apply speaker diarization
+
+        Parameters
+        ----------
+        current_file : `dict`
+            File as provided by a pyannote.database protocol.
+
+        Returns
+        -------
+        hypothesis : `pyannote.core.Annotation`
+            Speaker diarization output.
+        """
+
+        # segmentation into speech turns
+        speech_turns = self.speech_turn_segmentation(current_file)
+
+        # some files are only partially annotated and therefore one cannot
+        # evaluate speaker diarization results on the whole file.
+        # this option simply avoids trying to cluster those
+        # (potentially messy) un-annotated refions by focusing only on
+        # speech turns contained in the annotated regions.
+        if self.evaluation_only:
+            annotated = get_annotated(current_file)
+            speech_turns = speech_turns.crop(annotated, mode='intersection')
+
+        # in case there is one speech turn or less, there is no need to apply
+        # any kind of clustering approach.
+        if len(speech_turns) < 2:
+            return speech_turns
+
+        # split short/long speech turns. the idea is to first cluster long
+        # speech turns (i.e. those for which we can trust embeddings) and then
+        # assign each speech turn to the closest cluster.
+        long_speech_turns = speech_turns.empty()
+        shrt_speech_turns = speech_turns.empty()
+        for segment, track, label in speech_turns.itertracks(yield_label=True):
+            if segment.duration < self.min_duration:
+                shrt_speech_turns[segment, track] = label
+            else:
+                long_speech_turns[segment, track] = label
+
+        # in case there are no long speech turn to cluster, we return the
+        # original speech turns (= shrt_speech_turns)
+        if len(long_speech_turns) < 1:
+            return speech_turns
+
+        # first: cluster long speech turns
+        long_speech_turns = self.speech_turn_clustering(current_file,
+                                                        long_speech_turns)
+
+        # then: assign short speech turns to clusters
+        long_speech_turns.rename_labels(generator='string', copy=False)
+
+        if len(shrt_speech_turns) > 0:
+            shrt_speech_turns.rename_labels(generator='int', copy=False)
+            shrt_speech_turns = self.speech_turn_assignment(current_file,
+                                                            shrt_speech_turns,
+                                                            long_speech_turns)
+        # merge short/long speech turns
+        return long_speech_turns.update(
+            shrt_speech_turns, copy=False).support(collar=0.)
+
+        # TODO. add GMM-based resegmentation
+    def loss(self, current_file: dict, hypothesis: Annotation) -> float:
+        """Compute (1 - coverage) at target purity
+
+        If purity < target, return 1 + (1 - purity)
+
+        Parameters
+        ----------
+        current_file : `dict`
+            File as provided by a pyannote.database protocol.
+        hypothesis : `pyannote.core.Annotation`
+            Speech turns.
+
+        Returns
+        -------
+        error : `float`
+            1. - cluster coverage.
+        """
+        metric = DiarizationPurityCoverageFMeasure()
+        reference  = current_file['annotation']
+        uem = get_annotated(current_file)
+        f_measure = metric(reference, hypothesis, uem=uem)
+        purity, coverage, _ = metric.compute_metrics()
+        if purity > self.purity:
+            return 1. - coverage
+        else:
+            return 1. + (1. - purity)
+
+    #TODO : find out a way for loss and get_metric to cohabit
+    #def get_metric(self) -> GreedyDiarizationErrorRate:
+    #    """Return new instance of diarization error rate metric"""
+    #    return  GreedyDiarizationErrorRate(collar=0.0, skip_overlap=False)
 
 
 class Yin2018(SpeakerDiarization):
