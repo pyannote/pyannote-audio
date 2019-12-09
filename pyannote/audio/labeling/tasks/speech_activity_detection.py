b76efa831d9c95eac0c05d87aefc25bbb198de5c
Mymoza <marie-philippe.gill@hotmail.ca>
2019-12-09 11:55:50 -0500

    abstract class

--- pyannote/audio/labeling/tasks/speech_activity_detection.py
+++ pyannote/audio/labeling/tasks/speech_activity_detection.py
@@ -36,6 +36,7 @@ from .base import LabelingTaskGenerator
 from .base import TASK_MULTI_CLASS_CLASSIFICATION
 from ..gradient_reversal import GradientReversal
 from pyannote.audio.models.models import RNN
+from abc import ABC, abstractmethod
 
 class SpeechActivityDetectionGenerator(LabelingTaskGenerator):
     """Batch generator for training speech activity detection
@@ -152,7 +153,70 @@ class SpeechActivityDetection(LabelingTask):
             batch_size=self.batch_size,
             parallel=self.parallel) 
 
-class DomainAwareSpeechActivityDetection(SpeechActivityDetection):
+class DomainBranchSpeechActivityDetection(ABC):
+
+    @abstractmethod
+    def get_domain_scores(self, intermediate):
+        pass
+
+    def _batch_loss(self, batch):
+    """Helper function to performs the common operations required for the batch_loss function 
+
+    Parameters
+    ----------
+    batch : `dict`
+        ['X'] (`numpy.ndarray`)
+        ['y'] (`numpy.ndarray`)
+
+    Returns
+    -------
+    loss : Function f(input, target, weight=None) -> loss value
+    TO DO 
+    """
+    X = torch.tensor(batch['X'],
+                     dtype=torch.float32,
+                     device=self.device_)
+    fX, intermediate = self.model_(X, return_intermediate=self.attachment)
+
+    # speech activity detection
+    fX = fX.view((-1, self.n_classes_))
+    target = torch.tensor(
+        batch['y'],
+        dtype=torch.int64,
+        device=self.device_).contiguous().view((-1, ))
+
+    weight = self.weight
+    if weight is not None:
+        weight = weight.to(device=self.device_)
+    loss = self.loss_func_(fX, target, weight=weight)
+
+    # domain classification
+    domain_target = torch.tensor(
+        batch[self.domain],
+        dtype=torch.int64,
+        device=self.device_)
+
+    domain_scores = self.get_domain_scores(intermediate)
+    
+    # if gradient_reversal:
+    #     domain_scores = self.activation_(self.domain_classifier_(self.gradient_reversal_(intermediate)))
+    # else:
+    #     domain_scores = self.activation_(self.domain_classifier_(intermediate))
+
+    if self.domain_loss == "MSELoss":
+        # One hot encode domain_target for Mean Squared Error Loss
+        nb_domains = domain_scores.shape[1]
+        identity_mat = torch.sparse.torch.eye(nb_domains, device=self.device_)
+        domain_target = identity_mat.index_select(dim=0, index=domain_target)
+
+    
+    domain_loss = self.domain_loss_(domain_scores, domain_target)
+
+    return {'loss': loss + self.alpha * domain_loss,
+            'loss_domain': domain_loss,
+            'loss_task': loss}
+
+class DomainAwareSpeechActivityDetection(SpeechActivityDetection, DomainBranchSpeechActivityDetection):
     """Domain-aware speech activity detection
 
     Trains speech activity detection and domain classification jointly.
@@ -271,60 +335,11 @@ class DomainAwareSpeechActivityDetection(SpeechActivityDetection):
 
         super().save_epoch(epoch=epoch)
     
-    def _batch_loss(self, batch, gradient_reversal=False):
-        """Helper function to performs the common operations required for the batch_loss function 
 
-        Parameters
-        ----------
-        batch : `dict`
-            ['X'] (`numpy.ndarray`)
-            ['y'] (`numpy.ndarray`)
 
-        Returns
-        -------
-        loss : Function f(input, target, weight=None) -> loss value
-        TO DO 
-        """
-        X = torch.tensor(batch['X'],
-                         dtype=torch.float32,
-                         device=self.device_)
-        fX, intermediate = self.model_(X, return_intermediate=self.attachment)
-
-        # speech activity detection
-        fX = fX.view((-1, self.n_classes_))
-        target = torch.tensor(
-            batch['y'],
-            dtype=torch.int64,
-            device=self.device_).contiguous().view((-1, ))
-
-        weight = self.weight
-        if weight is not None:
-            weight = weight.to(device=self.device_)
-        loss = self.loss_func_(fX, target, weight=weight)
-
-        # domain classification
-        domain_target = torch.tensor(
-            batch[self.domain],
-            dtype=torch.int64,
-            device=self.device_)
-
-        if gradient_reversal:
-            domain_scores = self.activation_(self.domain_classifier_(self.gradient_reversal_(intermediate)))
-        else:
-            domain_scores = self.activation_(self.domain_classifier_(intermediate))
-
-        if self.domain_loss == "MSELoss":
-            # One hot encode domain_target for Mean Squared Error Loss
-            nb_domains = domain_scores.shape[1]
-            identity_mat = torch.sparse.torch.eye(nb_domains, device=self.device_)
-            domain_target = identity_mat.index_select(dim=0, index=domain_target)
-
-        
-        domain_loss = self.domain_loss_(domain_scores, domain_target)
-
-        return {'loss': loss + self.alpha * domain_loss,
-                'loss_domain': domain_loss,
-                'loss_task': loss}
+    def get_domain_scores(self, intermediate):
+        print("JAI ÉTÉ DANS L'ENFANNNNNNNNNNNNNNNT AWARE!!!!!!!!!!!!")
+        return domain_scores = self.activation_(self.domain_classifier_(intermediate))
 
     def batch_loss(self, batch):
         """Compute loss for current `batch`
@@ -343,7 +358,8 @@ class DomainAwareSpeechActivityDetection(SpeechActivityDetection):
 
         #domain_scores = self.activation_(self.domain_classifier_(intermediate)) 
 
-        return self._batch_loss(batch)
+        return super(DomainBranchSpeechActivityDetection)._batch_loss(batch)
+        #return self._batch_loss(batch)
 
         
 
@@ -388,11 +404,13 @@ class DomainAdversarialSpeechActivityDetection(DomainAwareSpeechActivityDetectio
             ['loss'] (`torch.Tensor`) : Loss
         """
 
-        return super()._batch_loss(batch, gradient_reversal=True)
+        return super()._batch_loss(batch)
 
         
 
-        
+    def get_domain_scores(self, intermediate):
+        print("JAI ÉTÉ DANS L'ENFANNNNNNNNNNNNNNNT ADVERSARIAL!!!!!!!!!!!!")
+        return self.activation_(self.domain_classifier_(self.gradient_reversal_(intermediate)))
 
         # return {'loss': loss + self.alpha * domain_loss,
         #         'loss_domain': domain_loss,
