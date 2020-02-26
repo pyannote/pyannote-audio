464c4e66abae670481fbf8f5a2bfabba727cf52c
Hervé Bredin <bredin@limsi.fr>
2020-02-26 16:15:23 +0100

    feat: add get_metric method to OverlapDetection pipeline

--- pyannote/audio/applications/overlap_detection.py
+++ pyannote/audio/applications/overlap_detection.py
@@ -29,28 +29,6 @@
 from pyannote.audio.pipeline.overlap_detection \
     import OverlapDetection as OverlapDetectionPipeline
 from .speech_detection import SpeechActivityDetection
-from pyannote.core import Timeline
-
 
 class OverlapDetection(SpeechActivityDetection):
-
     Pipeline = OverlapDetectionPipeline
-
-    def validate_init(self, protocol, subset='development'):
-
-        validation_data = super().validate_init(protocol, subset=subset)
-        for current_file in validation_data:
-
-            uri = current_file['uri']
-
-            # build overlap reference
-            overlap = Timeline(uri=uri)
-            turns = current_file['annotation']
-            for track1, track2 in turns.co_iter(turns):
-                if track1 == track2:
-                    continue
-                overlap.add(track1[0] & track2[0])
-            current_file['overlap'] = overlap.support().to_annotation()
-            # TODO. make 'annotated' focus on speech regions only
-
-        return validation_data
