7139fd2c99a9541a008b958c6232edcc85c824c4
Hervé BREDIN <hbredin@users.noreply.github.com>
2020-03-06 15:43:17 +0100

    doc: remove comment

--- pyannote/audio/labeling/tasks/base.py
+++ pyannote/audio/labeling/tasks/base.py
@@ -297,8 +297,7 @@ class LabelingTaskGenerator(BatchGenerator):
             uri = get_unique_identifier(current_file)
             if uri in self.data_:
                 self.data_[uri]['y'] = self.initialize_y(current_file)
-            # else: may issue some warning?
-
+                
         return sum(datum['duration'] for datum in self.data_.values())
 
     @property
