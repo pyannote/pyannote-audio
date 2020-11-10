2c46518655abe9af100bcbb9e551bcf0f4ce394c
Harry Coultas Blum <himion0@gmail.com>
2020-11-10 09:49:57 +0000

    Remove print statement
    Co-authored-by: Hervé BREDIN <hbredin@users.noreply.github.com>

--- tests/io_tests.py
+++ tests/io_tests.py
@@ -70,7 +70,6 @@ def test_crops_are_correct_shape():
     loader = Audio()
     shape = None
     for segment in SlidingWindow(end=secs):
-        print(segment)
         wav, sr = loader.crop({"waveform": waveform, "sample_rate": sr}, segment)
         if shape is None:
             shape = wav.shape
