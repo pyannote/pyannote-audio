e8b1d6d24e99ce86dc4cab0ef4687791517aef6a
Hervé Bredin <bredin@limsi.fr>
2020-12-17 14:09:05 +0100

    wip: update diarization pipeline to use new "Segmentation" task

--- pyannote/audio/pipelines/utils.py
+++ pyannote/audio/pipelines/utils.py
@@ -25,6 +25,7 @@ from typing import Union
 import numpy as np
 
 from pyannote.audio.core.inference import Inference
+from pyannote.audio.core.io import AudioFile
 from pyannote.core import Annotation, SlidingWindowFeature
 
 
@@ -61,7 +62,9 @@ def assert_int_labels(annotation: Annotation, name: str):
 
 
 def gather_label_embeddings(
-    annotation: Annotation, embeddings: Union[SlidingWindowFeature, Inference]
+    annotation: Annotation,
+    embeddings: Union[SlidingWindowFeature, Inference],
+    file: AudioFile = None,
 ):
     """Extract one embedding per label
 
@@ -72,6 +75,8 @@ def gather_label_embeddings(
     embeddings : SlidingWindowFeature or Inference
         Embeddings, either precomputed on a sliding window (SlidingWindowFeature)
         or to be computed on the fly (Inference).
+    file : AudioFile, optional
+        Needed when `embeddings` is an `Inference` instance
 
     Returns
     -------
@@ -108,7 +113,17 @@ def gather_label_embeddings(
             X.append(np.mean(x, axis=0))
 
         elif isinstance(embeddings, Inference):
-            # TODO: add "Timeline" chunk support to Inference.crop
-            raise NotImplementedError()
+
+            x = embeddings.crop(file, label_support)
+
+            # TODO: catch when label is so small that we can't extract embeddings
+
+            if embeddings.window == "sliding":
+                X.append(np.mean(x, axis=0))
+
+            elif embeddings.window == "whole":
+                X.append(x)
+
+            embedded_labels.append(label)
 
     return np.vstack(X), embedded_labels, skipped_labels
