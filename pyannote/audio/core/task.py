pyannote/pyannote-audio

commit e3347fab9abd3842d20ef8f31b0b0ff9cca44228

feat: add support for segmentation model warm up (#632)
* BREAKING: update Inference accordingly

pyannote/audio/core/task.py
@@ -28,7 +28,8 @@ import sys
 import warnings
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, Optional, Text
+from numbers import Number
+from typing import List, Optional, Text, Tuple, Union
 
 import pytorch_lightning as pl
 import torch
@@ -68,6 +69,14 @@ class Specifications:
     # chunk duration in seconds.
     # use None for variable-length chunks
     duration: Optional[float] = None
+
+    # use that many seconds on the left- and rightmost parts of each chunk
+    # to warm up the model. This is mostly useful for segmentation tasks.
+    # While the model does process those left- and right-most parts, only
+    # the remaining central part of each chunk is used for computing the
+    # loss during training, and for aggregating scores during inference.
+    # Defaults to 0. (i.e. no warm-up).
+    warm_up: Optional[Tuple[float, float]] = (0.0, 0.0)
 
     # (for classification tasks only) list of classes
     classes: Optional[List[Text]] = None
@@ -143,6 +152,13 @@ class Task(pl.LightningDataModule):
     min_duration : float, optional
         Sample training chunks duration uniformely between `min_duration`
         and `duration`. Defaults to `duration` (i.e. fixed length chunks).
+    warm_up : float or (float, float), optional
+        Use that many seconds on the left- and rightmost parts of each chunk
+        to warm up the model. This is mostly useful for segmentation tasks.
+        While the model does process those left- and right-most parts, only
+        the remaining central part of each chunk is used for computing the
+        loss during training, and for aggregating scores during inference.
+        Defaults to 0. (i.e. no warm-up).
     batch_size : int, optional
         Number of training samples per batch. Defaults to 32.
     num_workers : int, optional
@@ -169,6 +185,7 @@ class Task(pl.LightningDataModule):
         protocol: Protocol,
         duration: float = 2.0,
         min_duration: float = None,
+        warm_up: Union[float, Tuple[float, float]] = 0.0,
         batch_size: int = 32,
         num_workers: int = None,
         pin_memory: bool = False,
@@ -183,6 +200,11 @@ class Task(pl.LightningDataModule):
         self.duration = duration
         self.min_duration = duration if min_duration is None else min_duration
         self.batch_size = batch_size
+
+        # training
+        if isinstance(warm_up, Number):
+            warm_up = (warm_up, warm_up)
+        self.warm_up = warm_up
 
         # multi-processing
         if num_workers is None:
@@ -341,15 +363,27 @@ class Task(pl.LightningDataModule):
 
         # forward pass
         y_pred = self.model(batch["X"])
-        # (batch_size, num_frames, num_classes)
+
+        if self.is_multi_task:
+            _, any_y_pred = next(iter(y_pred.items()))
+            batch_size, num_frames, _ = any_y_pred.shape
+        else:
+            batch_size, num_frames, _ = y_pred.shape
+            # (batch_size, num_frames, num_classes)
 
         # target
         y = batch["y"]
 
         # frames weight
         weight_key = getattr(self, "weight", None) if stage == "train" else None
-        weight = batch.get(weight_key, None)
+        weight = batch.get(weight_key, torch.ones(batch_size, num_frames, 1))
         # (batch_size, num_frames, 1)
+
+        # warm-up
+        warm_up_left = round(self.warm_up[0] / self.duration * num_frames)
+        weight[:, :warm_up_left] = 0.0
+        warm_up_right = round(self.warm_up[1] / self.duration * num_frames)
+        weight[:, num_frames - warm_up_right :] = 0.0
 
         # compute multi-task loss as the sum of loss of each task
         if self.is_multi_task:

pyannote/audio/tasks/segmentation/segmentation.py
@@ -22,7 +22,7 @@
 
 import math
 from collections import Counter
-from typing import Text
+from typing import Text, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -51,6 +51,12 @@ class Segmentation(SegmentationTaskMixin, Task):
         pyannote.database protocol
     duration : float, optional
         Chunks duration. Defaults to 2s.
+    warm_up : float or (float, float), optional
+        Use that many seconds on the left- and rightmost parts of each chunk
+        to warm up the model. While the model does process those left- and right-most
+        parts, only the remaining central part of each chunk is used for computing the
+        loss during training, and for aggregating scores during inference.
+        Defaults to 0. (i.e. no warm-up).
     balance: str, optional
         When provided, training samples are sampled uniformly with respect to that key.
         For instance, setting `balance` to "uri" will make sure that each file will be
@@ -89,6 +95,7 @@ class Segmentation(SegmentationTaskMixin, Task):
         self,
         protocol: Protocol,
         duration: float = 2.0,
+        warm_up: Union[float, Tuple[float, float]] = 0.0,
         overlap: dict = OVERLAP_DEFAULTS,
         balance: Text = None,
         weight: Text = None,
@@ -103,6 +110,7 @@ class Segmentation(SegmentationTaskMixin, Task):
         super().__init__(
             protocol,
             duration=duration,
+            warm_up=warm_up,
             batch_size=batch_size,
             num_workers=num_workers,
             pin_memory=pin_memory,
@@ -163,6 +171,7 @@ class Segmentation(SegmentationTaskMixin, Task):
                 problem=Problem.MULTI_LABEL_CLASSIFICATION,
                 resolution=Resolution.FRAME,
                 duration=self.duration,
+                warm_up=self.warm_up,
                 classes=[f"speaker#{i+1}" for i in range(self.num_speakers)],
                 permutation_invariant=True,
             )
@@ -301,6 +310,7 @@ class Segmentation(SegmentationTaskMixin, Task):
 
         # forward pass
         prediction = self.model(batch["X"])
+        batch_size, num_frames, _ = prediction.shape
         # (batch_size, num_frames, num_classes)
 
         # target
@@ -310,8 +320,14 @@ class Segmentation(SegmentationTaskMixin, Task):
 
         # frames weight
         weight_key = getattr(self, "weight", None)
-        weight = batch.get(weight_key, None)
+        weight = batch.get(weight_key, torch.ones(batch_size, num_frames, 1))
         # (batch_size, num_frames, 1)
+
+        # warm-up
+        warm_up_left = round(self.warm_up[0] / self.duration * num_frames)
+        weight[:, :warm_up_left] = 0.0
+        warm_up_right = round(self.warm_up[1] / self.duration * num_frames)
+        weight[:, num_frames - warm_up_right :] = 0.0
 
         seg_loss = self.segmentation_loss(permutated_prediction, target, weight=weight)
 
@@ -372,13 +388,17 @@ class Segmentation(SegmentationTaskMixin, Task):
         # y = (batch_size, num_frames, num_classes)
 
         y_pred = self.model(X)
+        _, num_frames, _ = y_pred.shape
+        # y_pred = (batch_size, num_frames, num_classes)
 
         permutated_y_pred, _ = permutate(y, y_pred)
 
-        # y_pred = (batch_size, num_frames, num_classes)
+        warm_up_left = round(self.warm_up[0] / self.duration * num_frames)
+        warm_up_right = round(self.warm_up[1] / self.duration * num_frames)
 
         val_fbeta = self.val_fbeta(
-            permutated_y_pred[:, ::10].squeeze(), y[:, ::10].squeeze()
+            y_pred[:, warm_up_left : num_frames - warm_up_right : 10].squeeze(),
+            y[:, warm_up_left : num_frames - warm_up_right : 10].squeeze(),
         )
         self.model.log(
             f"{self.ACRONYM}@val_fbeta",
@@ -439,6 +459,10 @@ class Segmentation(SegmentationTaskMixin, Task):
             # plot prediction
             ax_hyp = axes[row_idx * 4 + 2, col_idx]
             sample_y_pred = y_pred[sample_idx]
+            ax_hyp.axvspan(0, warm_up_left, color="k", alpha=0.5, lw=0)
+            ax_hyp.axvspan(
+                num_frames - warm_up_right, num_frames, color="k", alpha=0.5, lw=0
+            )
             ax_hyp.plot(sample_y_pred)
             ax_hyp.set_ylim(-0.1, 1.1)
             ax_hyp.set_xlim(0, len(sample_y))
@@ -447,6 +471,10 @@ class Segmentation(SegmentationTaskMixin, Task):
             # plot permutated prediction
             ax_map = axes[row_idx * 4 + 3, col_idx]
             sample_y_pred_map = permutated_y_pred[sample_idx]
+            ax_map.axvspan(0, warm_up_left, color="k", alpha=0.5, lw=0)
+            ax_map.axvspan(
+                num_frames - warm_up_right, num_frames, color="k", alpha=0.5, lw=0
+            )
             ax_map.plot(sample_y_pred_map)
             ax_map.set_ylim(-0.1, 1.1)
             ax_map.set_xlim(0, len(sample_y))

pyannote/audio/cli/train_config/task/OverlappedSpeechDetection.yaml
@@ -1,6 +1,7 @@
 # @package _group_
 _target_: pyannote.audio.tasks.OverlappedSpeechDetection
 duration: 3.0
+warm_up: 0.0
 balance: null
 overlap:
   probability: 0.5

pyannote/audio/cli/train_config/task/Segmentation.yaml
@@ -1,6 +1,7 @@
 # @package _group_
 _target_: pyannote.audio.tasks.Segmentation
 duration: 5.0
+warm_up: 0.0
 balance: null
 overlap:
   probability: 0.5

pyannote/audio/cli/train_config/task/VoiceActivityDetection.yaml
@@ -1,6 +1,7 @@
 # @package _group_
 _target_: pyannote.audio.tasks.VoiceActivityDetection
 duration: 3.0
+warm_up: 0.0
 balance: null
 weight: null
 batch_size: 32

pyannote/audio/core/inference.py
@@ -44,8 +44,6 @@ TaskName = Union[Text, None]
 class Inference:
     """Inference
 
-    TODO: add support for model averaging (either at output or weight level)
-
     Parameters
     ----------
     model : Model
@@ -59,8 +57,8 @@ class Inference:
         Chunk duration, in seconds. Defaults to duration used for training the model.
         Has no effect when `window` is "whole".
     step : float, optional
-        Step between consecutive chunks, in seconds. Defaults to 10% of duration.
-        Has no effect when `window` is "whole".
+        Step between consecutive chunks, in seconds. Defaults to warm-up duration when
+        greater than 0s, otherwise 10% of duration. Has no effect when `window` is "whole".
     batch_size : int, optional
         Batch size. Larger values make inference faster. Defaults to 32.
     device : torch.device, optional
@@ -141,9 +139,16 @@ class Inference:
             )
         self.duration = duration
 
+        self.warm_up = specifications.warm_up
+        # Use that many seconds on the left- and rightmost parts of each chunk
+        # to warm up the model. While the model does process those left- and right-most
+        # parts, only the remaining central part of each chunk is used for aggregating
+        # scores during inference.
+
         # step between consecutive chunks
         if step is None:
-            step = 0.1 * self.duration
+            step = 0.1 * self.duration if self.warm_up[0] == 0.0 else self.warm_up[0]
+
         if step > self.duration:
             raise ValueError(
                 f"Step between consecutive chunks is set to {step:g}s, while chunks are "
@@ -340,6 +345,19 @@ class Inference:
             # Hamming window used for overlap-add aggregation
             hamming = np.hamming(num_frames_per_chunk).reshape(-1, 1)
 
+            # warm-up window used for overlap-add aggregation
+            warm_up = np.ones((num_frames_per_chunk, 1))
+            warm_up_first = np.ones((num_frames_per_chunk, 1))
+            warm_up_last = np.ones((num_frames_per_chunk, 1))
+            warm_up_left = round(self.warm_up[0] / self.duration * num_frames_per_chunk)
+            warm_up_right = round(
+                self.warm_up[1] / self.duration * num_frames_per_chunk
+            )
+            warm_up[:warm_up_left] = 0.0
+            warm_up_last[:warm_up_left] = 0.0
+            warm_up[num_frames_per_chunk - warm_up_right :] = 0.0
+            warm_up_first[num_frames_per_chunk - warm_up_right :] = 0.0
+
             # aggregated_output[i] will be used to store the (hamming-weighted) sum
             # of all predictions for frame #i
             aggregated_output: np.ndarray = np.zeros(
@@ -353,8 +371,13 @@ class Inference:
             )
 
             if specifications.permutation_invariant:
-                # previous outputs that overlap with current output by at least 50%
-                num_overlap = math.floor(0.5 * self.duration / self.step)
+                # number of previous outputs that overlap with current one by at least 50% of their warmed up region
+                num_overlap = math.floor(
+                    0.5
+                    * (self.duration - self.warm_up[0] - self.warm_up[1])
+                    / self.step
+                )
+                # keep track of those previous outputs in a "deque"
                 if num_overlap > 0:
                     previous_outputs: Deque[np.ndarray] = deque([], maxlen=num_overlap)
 
@@ -370,13 +393,29 @@ class Inference:
                         )
                     previous_outputs.append(output)
 
+                # when processing first chunk, do not weigh-down its left-most side
+                if c == 0:
+                    # unless there is just one chunk, where we do not weigh-down any side
+                    if not has_last_chunk and num_chunks == 1:
+                        warm_up_ = 1.0
+                    else:
+                        warm_up_ = warm_up_first
+
+                # when processing last chunk, make sure to not weigh-down its right-most side
+                elif not has_last_chunk and c + 1 == num_chunks:
+                    warm_up_ = warm_up_last
+
+                # when processing an internal chunk, weigh-down both sides
+                else:
+                    warm_up_ = warm_up
+
                 aggregated_output[start_frame : start_frame + num_frames_per_chunk] += (
-                    output * hamming
+                    output * hamming * warm_up_
                 )
 
                 overlapping_chunk_count[
                     start_frame : start_frame + num_frames_per_chunk
-                ] += hamming
+                ] += (hamming * warm_up_)
 
             # process last (right-aligned) chunk separately
             if has_last_chunk:
@@ -394,9 +433,11 @@ class Inference:
                     )
 
                 aggregated_output[-num_frames_per_chunk:] += (
-                    last_output[task_name] * hamming
+                    last_output[task_name] * hamming * warm_up_last
                 )
-                overlapping_chunk_count[-num_frames_per_chunk:] += hamming
+                overlapping_chunk_count[-num_frames_per_chunk:] += (
+                    hamming * warm_up_last
+                )
 
             aggregated_output /= np.maximum(overlapping_chunk_count, 1e-12)
 
@@ -420,7 +461,7 @@ class Inference:
 
         Parameters
         ----------
-        past_outputs : deque of(num_frames, num_classes) np.ndarray
+        past_outputs : deque of (num_frames, num_classes) np.ndarray
             Previous output
         output : (num_frames, num_classes) np.ndarray
             Current output
@@ -434,13 +475,18 @@ class Inference:
             Permutated current output.
         """
 
-        num_frames, num_classes = output.shape
+        num_frames, _ = output.shape
+        warm_up_left = round(self.warm_up[0] / self.duration * num_frames)
+        warm_up_right = round(self.warm_up[1] / self.duration * num_frames)
 
         permutations = []
         for o, past_output in enumerate(reversed(past_outputs)):
             permutation = permutate(
-                past_output[np.newaxis, (o + 1) * step_size :],
-                output[: num_frames - (o + 1) * step_size],
+                past_output[
+                    np.newaxis,
+                    warm_up_left + (o + 1) * step_size : num_frames - warm_up_right,
+                ],
+                output[warm_up_left : num_frames - warm_up_right - (o + 1) * step_size],
             )[1][0]
             permutations.append(permutation)
 
@@ -533,6 +579,13 @@ class Inference:
         -----
         If model has several outputs (multi-task), those will be returned as a
         {task_name: output} dictionary.
+
+        If model needs to be warmed up, remember to extend the requested chunk with the
+        corresponding amount of time so that it is actually warmed up when processing the
+        chunk of interest:
+        >>> chunk_of_interest = Segment(10, 15)
+        >>> extended_chunk = Segment(10 - warm_up, 15 + warm_up)
+        >>> inference.crop(file, extended_chunk).crop(chunk_of_interest, returns_data=False)
         """
 
         if self.window == "sliding":

pyannote/audio/tasks/segmentation/mixins.py
@@ -483,9 +483,16 @@ class SegmentationTaskMixin:
         # y = (batch_size, num_frames, num_classes)
 
         y_pred = self.model(X)
+        _, num_frames, _ = y_pred.shape
         # y_pred = (batch_size, num_frames, num_classes)
 
-        val_fbeta = self.val_fbeta(y_pred[:, ::10].squeeze(), y[:, ::10].squeeze())
+        warm_up_left = round(self.warm_up[0] / self.duration * num_frames)
+        warm_up_right = round(self.warm_up[1] / self.duration * num_frames)
+
+        val_fbeta = self.val_fbeta(
+            y_pred[:, warm_up_left : num_frames - warm_up_right : 10].squeeze(),
+            y[:, warm_up_left : num_frames - warm_up_right : 10].squeeze(),
+        )
         self.model.log(
             f"{self.ACRONYM}@val_fbeta",
             val_fbeta,
@@ -544,6 +551,10 @@ class SegmentationTaskMixin:
             # plot prediction
             ax_hyp = axes[row_idx * 3 + 2, col_idx]
             sample_y_pred = y_pred[sample_idx]
+            ax_hyp.axvspan(0, warm_up_left, color="k", alpha=0.5, lw=0)
+            ax_hyp.axvspan(
+                num_frames - warm_up_right, num_frames, color="k", alpha=0.5, lw=0
+            )
             ax_hyp.plot(sample_y_pred)
             ax_hyp.set_ylim(-0.1, 1.1)
             ax_hyp.set_xlim(0, len(sample_y))

pyannote/audio/tasks/segmentation/overlapped_speech_detection.py
@@ -21,7 +21,7 @@
 # SOFTWARE.
 
 
-from typing import Text
+from typing import Text, Tuple, Union
 
 import numpy as np
 from torch_audiomentations.core.transforms_interface import BaseWaveformTransform
@@ -51,6 +51,12 @@ class OverlappedSpeechDetection(SegmentationTaskMixin, Task):
         pyannote.database protocol
     duration : float, optional
         Chunks duration. Defaults to 2s.
+    warm_up : float or (float, float), optional
+        Use that many seconds on the left- and rightmost parts of each chunk
+        to warm up the model. While the model does process those left- and right-most
+        parts, only the remaining central part of each chunk is used for computing the
+        loss during training, and for aggregating scores during inference.
+        Defaults to 0. (i.e. no warm-up).
     balance: str, optional
         When provided, training samples are sampled uniformly with respect to that key.
         For instance, setting `balance` to "uri" will make sure that each file will be
@@ -87,6 +93,7 @@ class OverlappedSpeechDetection(SegmentationTaskMixin, Task):
         self,
         protocol: Protocol,
         duration: float = 2.0,
+        warm_up: Union[float, Tuple[float, float]] = 0.0,
         overlap: dict = OVERLAP_DEFAULTS,
         balance: Text = None,
         weight: Text = None,
@@ -99,6 +106,7 @@ class OverlappedSpeechDetection(SegmentationTaskMixin, Task):
         super().__init__(
             protocol,
             duration=duration,
+            warm_up=warm_up,
             batch_size=batch_size,
             num_workers=num_workers,
             pin_memory=pin_memory,
@@ -109,6 +117,7 @@ class OverlappedSpeechDetection(SegmentationTaskMixin, Task):
             problem=Problem.BINARY_CLASSIFICATION,
             resolution=Resolution.FRAME,
             duration=self.duration,
+            warm_up=self.warm_up,
             classes=[
                 "overlap",
             ],

commit 338bd20675b56cddf80eea2f6137ab764be5bbee

fix: fix bugs introduced by PR #632

pyannote/audio/core/task.py
@@ -376,7 +376,10 @@ class Task(pl.LightningDataModule):
 
         # frames weight
         weight_key = getattr(self, "weight", None) if stage == "train" else None
-        weight = batch.get(weight_key, torch.ones(batch_size, num_frames, 1))
+        weight = batch.get(
+            weight_key,
+            torch.ones(batch_size, num_frames, 1, device=self.model.device),
+        )
         # (batch_size, num_frames, 1)
 
         # warm-up

pyannote/audio/tasks/segmentation/segmentation.py
@@ -320,7 +320,10 @@ class Segmentation(SegmentationTaskMixin, Task):
 
         # frames weight
         weight_key = getattr(self, "weight", None)
-        weight = batch.get(weight_key, torch.ones(batch_size, num_frames, 1))
+        weight = batch.get(
+            weight_key,
+            torch.ones(batch_size, num_frames, 1, device=self.model.device),
+        )
         # (batch_size, num_frames, 1)
 
         # warm-up
@@ -397,7 +400,9 @@ class Segmentation(SegmentationTaskMixin, Task):
         warm_up_right = round(self.warm_up[1] / self.duration * num_frames)
 
         val_fbeta = self.val_fbeta(
-            y_pred[:, warm_up_left : num_frames - warm_up_right : 10].squeeze(),
+            permutated_y_pred[
+                :, warm_up_left : num_frames - warm_up_right : 10
+            ].squeeze(),
             y[:, warm_up_left : num_frames - warm_up_right : 10].squeeze(),
         )
         self.model.log(