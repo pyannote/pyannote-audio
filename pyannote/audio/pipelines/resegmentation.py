2abdb9a2626858d8035247ee8d73a97808aecc9b
Hervé Bredin <bredin@limsi.fr>
2021-03-17 15:27:59 +0100

    wip: concatenate instead of aggregate

--- pyannote/audio/pipelines/resegmentation.py
+++ pyannote/audio/pipelines/resegmentation.py
@@ -51,6 +51,12 @@ class BasicResegmentation(Pipeline):
 
     Hyper-parameters
     ----------------
+    onset, offset : float
+        Onset/offset detection thresholds
+    min_duration_on : float
+        Remove speaker turn shorter than that many seconds.
+    min_duration_off : float
+        Fill same-speaker gaps shorter than that many seconds.
 
     """
 
@@ -170,7 +176,7 @@ class BasicResegmentation(Pipeline):
         aggregated = np.zeros((num_frames_in_file, num_clusters))
         overlapped = np.zeros((num_frames_in_file, num_clusters))
 
-        for chunk, segmentation in segmentations:
+        for c, (chunk, segmentation) in enumerate(segmentations):
 
             # only consider active speakers in `segmentation`
             active = np.max(segmentation, axis=0) > self.onset
@@ -189,10 +195,53 @@ class BasicResegmentation(Pipeline):
             )
 
             start_frame = round(chunk.start / self.seg_frame_duration_)
-            aggregated[
-                start_frame : start_frame + num_frames_in_chunk
-            ] += permutated_segmentation
-            overlapped[start_frame : start_frame + num_frames_in_chunk] += 1.0
+
+            window = np.zeros(num_frames_in_chunk, 1)
+            if c == 0:
+                window[
+                    : math.ceil(
+                        0.5
+                        * (
+                            self.seg_chunk_duration_
+                            + self.segmentation_inference_by_chunk_.step
+                        )
+                        / self.seg_frame_duration_
+                    )
+                ] = 1
+            elif c + 1 == num_chunks:
+                window[
+                    math.floor(
+                        0.5
+                        * (
+                            self.seg_chunk_duration_
+                            - self.segmentation_inference_by_chunk_.step
+                        )
+                        / self.seg_frame_duration_
+                    ) :
+                ] = 1
+            else:
+                window[
+                    math.floor(
+                        0.5
+                        * (
+                            self.seg_chunk_duration_
+                            - self.segmentation_inference_by_chunk_.step
+                        )
+                        / self.seg_frame_duration_
+                    ) : math.ceil(
+                        0.5
+                        * (
+                            self.seg_chunk_duration_
+                            + self.segmentation_inference_by_chunk_.step
+                        )
+                        / self.seg_frame_duration_
+                    )
+                ] = 1
+
+            aggregated[start_frame : start_frame + num_frames_in_chunk] += (
+                permutated_segmentation * window
+            )
+            overlapped[start_frame : start_frame + num_frames_in_chunk] += window
 
         speaker_activations = SlidingWindowFeature(
             aggregated / overlapped, frames, labels=labels
