f9fee65b9a35708d0590e6bf91e42a2212ab3e60
J-Petiot <jimflorent@yahoo.fr>
2021-10-05 10:42:58 +0200

    Change as per hbredin's comments V03 -> voice_activity_detection Segmentation Model & Hyper_parameters as parameters to the recipe

--- pyannote/audio/interactive/voice_activity_detection.py
+++ pyannote/audio/interactive/voice_activity_detection.py
@@ -1,50 +1,14 @@
 from copy import deepcopy
 from pathlib import Path
-from typing import Any, Dict, Iterable, List, Optional, Union
+from typing import Any, Dict, Iterable, Optional, Union
 
 import prodigy
-from answers2file import answers2file
-from prodigy.components.db import connect
-from prodigy.components.loaders import Audio
-from prodigy.core import recipe
-from prodigy.util import log
+from prodigy.components.loaders import Audio as AudioLoader
 from utils import SAMPLE_RATE, chunks, normalize, to_audio_spans, to_base64
 
-from pyannote.audio.core.io import Audio as AudioFile
+from pyannote.audio.core.io import Audio
 from pyannote.audio.pipelines import VoiceActivityDetection
 from pyannote.core import Annotation, Segment
-from pyannote.pipeline import Optimizer
-
-# IMPORT POUR SAD
-
-
-db = connect("sqlite", {})
-
-# Global Model
-pipeline = VoiceActivityDetection(segmentation="pyannote/segmentation", step=0.5)
-HYPER_PARAMETERS = {
-    "onset": 0.5,
-    "offset": 0.5,
-    "min_duration_on": 0.0,
-    "min_duration_off": 0.0,
-}
-pipeline.instantiate(HYPER_PARAMETERS)
-
-
-def update(answers):
-    prodigy.log("---- UPDATE ----")
-    annotations = db.get_dataset("asupprimer")
-    validation_files = answers2file(annotations)
-    optimizer = Optimizer(pipeline)
-    bp = optimizer.tune(
-        validation_files,
-        n_iterations=2,
-        show_progress=False,
-        warm_start=pipeline.parameters(instantiated=True),
-    )
-    pipeline.instantiate(bp["params"])
-    prodigy.log(bp)
-    prodigy.log("---- END UPDATE ----")
 
 
 def remove_base64(examples):
@@ -57,17 +21,17 @@ def remove_base64(examples):
     return examples
 
 
-def sad_manual_stream(
+def voice_activity_detection_stream(
     pipeline: VoiceActivityDetection, source: Path, chunk: float = 10.0
 ) -> Iterable[Dict]:
-    """Stream for pyannote.sad.manual recipe
+    """
+    Stream for pyannote.voice_activity_detection recipe
     Applies (pretrained) speech activity detection and sends the results for
     manual correction chunk by chunk.
     Parameters
     ----------
-    pipeline : InteractiveDiarization
-        Pretrained speaker diarization interactive pipeline.
-        Note that only the speech activity detection part is used.
+    pipeline : VoiceActivityDetection
+        Pretrained speech activity detection pipeline.
     source : Path
         Directory containing audio files to process.
     chunk : float, optional
@@ -83,12 +47,10 @@ def sad_manual_stream(
         "audio_spans" : speech spans detected by pretrained SAD model
         "audio_spans_original" : copy of "audio_spans"
         "meta" : additional meta-data displayed in Prodigy UI
-        "recipe" : "pyannote.sad.manual"
     """
-    raw_audio = AudioFile(sample_rate=SAMPLE_RATE, mono=True)
-    # print(pipeline.onset)
+    raw_audio = Audio(sample_rate=SAMPLE_RATE, mono=True)
 
-    for audio_source in Audio(source):
+    for audio_source in AudioLoader(source):
 
         path = audio_source["path"]
         text = audio_source["text"]
@@ -115,8 +77,6 @@ def sad_manual_stream(
                 "audio_spans_original": deepcopy(task_audio_spans),
                 "chunk": {"start": 0, "end": duration},
                 "meta": {"file": text},
-                # this is needed by other recipes
-                "recipe": "pyannote.sad.manual",
             }
 
         else:
@@ -141,13 +101,11 @@ def sad_manual_stream(
                         "start": f"{focus.start:.1f}",
                         "end": f"{focus.end:.1f}",
                     },
-                    # this is needed by other recipes
-                    "recipe": "pyannote.sad.manual",
                 }
 
 
-@recipe(
-    "audio.v03",
+@prodigy.recipe(
+    "pyannote.voice_activity_detection",
     dataset=("Dataset to save annotations to", "positional", None, str),
     source=(
         "Data to annotate (file path or '-' to read from standard input)",
@@ -162,32 +120,31 @@ def sad_manual_stream(
         float,
     ),
 )
-def v03(
+def voice_activity_detection(
     dataset: str,
     source: Union[str, Iterable[dict]],
-    loader: Optional[str] = "audio",
     chunk: float = 10.0,
-    autoplay: bool = False,
-    keep_base64: bool = False,
-    fetch_media: bool = False,
-    exclude: Optional[List[str]] = None,
+    segmentation_model: Optional[str] = "pyannote/segmentation",
+    hyper_parameters: Optional[dict] = {
+        "onset": 0.5,
+        "offset": 0.5,
+        "min_duration_on": 0.0,
+        "min_duration_off": 0.0,
+    },
 ) -> Dict[str, Any]:
 
-    log("RECIPE: Starting recipe audio.v03", locals())
-    label = ["Speech"]
+    pipeline = VoiceActivityDetection(segmentation=segmentation_model, step=0.5)
+    pipeline.instantiate(hyper_parameters)
+    prodigy.log("RECIPE: Starting recipe voice_activity_detection", locals())
 
     return {
         "view_id": "audio_manual",
         "dataset": dataset,
-        "stream": sad_manual_stream(pipeline, source, chunk=chunk),
-        "before_db": remove_base64 if not keep_base64 else None,
-        "exclude": exclude,
-        "update": update,
+        "stream": voice_activity_detection_stream(pipeline, source, chunk=chunk),
+        "before_db": remove_base64,
         "config": {
-            "labels": label,
-            "audio_autoplay": autoplay,
-            "force_stream_order": False,
+            "labels": ["Speech"],
+            "audio_autoplay": True,
             "show_audio_minimap": False,
-            "instant_submit": True,
         },
     }
