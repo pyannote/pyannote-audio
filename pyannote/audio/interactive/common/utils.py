c2cbbbb8979c074898516e5f043a8553a9e8b1c7
Hervé Bredin <bredin@limsi.fr>
2022-02-17 16:14:58 +0100

    chore: improve main recipe code base

--- pyannote/audio/interactive/common/utils.py
+++ pyannote/audio/interactive/common/utils.py
@@ -22,31 +22,33 @@
 
 import base64
 import io
-import random
-from typing import Dict, Iterator, List, Text
+from typing import Dict, List, Text
 
 import numpy as np
 import scipy.io.wavfile
 
 from pyannote.core import Annotation, Segment, SlidingWindow
+from pyannote.audio import Audio
 
-SAMPLE_RATE = 16000
-Time = float
-
-
-def normalize(waveform: np.ndarray) -> np.ndarray:
-    """Normalize waveform for better display in Prodigy UI"""
-    return waveform / (np.max(np.abs(waveform)) + 1e-8)
+from pathlib import Path
+from typing import Optional
+from prodigy.components.loaders import Audio as ProdigyAudioLoader
 
 
-def to_base64(waveform: np.ndarray, sample_rate: int = SAMPLE_RATE) -> Text:
-    """Convert waveform to base64 data"""
-    with io.BytesIO() as content:
-        scipy.io.wavfile.write(content, sample_rate, waveform)
-        content.seek(0)
-        b64 = base64.b64encode(content.read()).decode()
-        b64 = f"data:audio/x-wav;base64,{b64}"
-    return b64
+class AudioForProdigy(Audio):
+    def __init__(self):
+        super().__init__(sample_rate=16000, mono=True)
+
+    def crop(self, path: Path, excerpt: Segment) -> Text:
+        waveform, _ = super().crop(path, excerpt)
+        waveform = waveform.numpy().T
+        waveform /= np.max(np.abs(waveform)) + 1e-8
+        with io.BytesIO() as content:
+            scipy.io.wavfile.write(content, self.sample_rate, waveform)
+            content.seek(0)
+            b64 = base64.b64encode(content.read()).decode()
+            b64 = f"data:audio/x-wav;base64,{b64}"
+        return b64
 
 
 def to_audio_spans(annotation: Annotation, focus: Segment = None) -> Dict:
@@ -70,6 +72,22 @@ def to_audio_spans(annotation: Annotation, focus: Segment = None) -> Dict:
     ]
 
 
+def get_audio_spans(
+    annotation: Annotation, excerpt: Segment, excerpt_with_context: Segment = None
+):
+
+    excerpt_with_context = excerpt_with_context or excerpt
+    shift = excerpt.start - excerpt_with_context.start
+
+    shifted_excerpt = Segment(start=shift, end=shift + excerpt.duration)
+    excerpt_annotation = annotation.crop(shifted_excerpt)
+
+    return [
+        {"start": segment.start - shift, "end": segment.end - shift, "label": label}
+        for segment, _, label in excerpt_annotation.itertracks(yield_label=True)
+    ]
+
+
 def remove_audio_before_db(examples: List[Dict]) -> List[Dict]:
     """Remove (potentially heavy) 'audio' key from examples
 
@@ -90,36 +108,51 @@ def remove_audio_before_db(examples: List[Dict]) -> List[Dict]:
     return examples
 
 
-def chunks(
-    duration: float, chunk: float = 30, shuffle: bool = False
-) -> Iterator[Segment]:
-    """Partition [0, duration] time range into smaller chunks
+def get_chunks(source: Path, chunk_duration: Optional[float] = None):
 
-    Parameters
-    ----------
-    duration : float
-        Total duration, in seconds.
-    chunk : float, optional
-        Chunk duration, in seconds. Defaults to 30.
-    shuffle : bool, optional
-        Yield chunks in random order. Defaults to chronological order.
+    files = ProdigyAudioLoader(source)
+    audio = Audio()
 
-    Yields
-    ------
-    focus : Segment
+    for file in files:
+
+        duration = audio.get_duration(file["path"])
+
+        if (chunk_duration is None) or (duration < chunk_duration):
+            chunks = [Segment(0, duration)]
+        else:
+            windows = SlidingWindow(
+                start=0.0, duration=chunk_duration, step=chunk_duration
+            )
+            chunks = windows(Segment(0, duration), align_last=True)
+
+        for chunk in chunks:
+            yield file, chunk
+
+
+def before_db(examples):
+    """Post-process examples before sending them to the database
+    
+    1. Remove "audio" key as it is very heavy and can easily be retrieved from other keys
+    2. Shift Prodigy/wavesurfer chunk-based audio spans so that their timing are file-based.
     """
 
-    sliding_window = SlidingWindow(start=0.0, step=chunk, duration=chunk)
-    whole = Segment(0, duration)
+    for eg in examples:
 
-    if shuffle:
-        chunks_ = list(chunks(duration, chunk=chunk, shuffle=False))
-        random.shuffle(chunks_)
-        for chunk in chunks_:
-            yield chunk
+        # 1. remove "audio" key as it can be retrieved from "chunk" and "path" keys
+        if "audio" in eg:
+            del eg["audio"]
 
-    else:
-        for window in sliding_window(whole):
-            yield window
-        if window.end < duration:
-            yield Segment(window.end, duration)
+        # 2. shift audio spans
+        chunk_start = eg["chunk"]["start"]
+        audio_spans_keys = [key for key in eg if "audio_spans" in key]
+        for key in audio_spans_keys:
+            eg[key] = [
+                {
+                    "start": span["start"] + chunk_start,
+                    "end": span["end"] + chunk_start,
+                    "label": span["label"],
+                }
+                for span in eg[key]
+            ]
+
+    return examples
--- pyannote/audio/interactive/pipeline/recipe.py
+++ pyannote/audio/interactive/pipeline/recipe.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2021 CNRS
+# Copyright (c) 2021-2022 CNRS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,150 +20,119 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# AUTHORS
+# Jim Petiot
+# Hervé Bredin
+
 import base64
 import os
+from tempfile import mkstemp
 from collections.abc import Iterator
 from copy import deepcopy
 from pathlib import Path
-from typing import Any, Dict, Iterable, Union
+from typing import Any, Dict, Iterable, List, Union
+import random
 
 import prodigy
-import torch.nn.functional as F
-from prodigy.components.loaders import Audio as AudioLoader
 
-from pyannote.audio import Pipeline
-from pyannote.audio.core.io import Audio
+from pyannote.audio import Audio, Pipeline
 from pyannote.core import Annotation, Segment
 
 from ..common.utils import (
-    SAMPLE_RATE,
-    chunks,
-    normalize,
-    remove_audio_before_db,
-    to_audio_spans,
-    to_base64,
+    get_chunks,
+    before_db,
+    get_audio_spans,
+    AudioForProdigy,
 )
 
 
-def general_stream(
-    pipeline: Pipeline, source: Path, labels: [dict], chunk: float = 10.0,
+def stream(
+    pipeline: Pipeline,
+    source: Path,
+    labels: List[str],
+    chunk: float = 30.0,
+    randomize: bool = False,
 ) -> Iterable[Dict]:
     """Stream for `pyannote.audio` recipe
 
-    Applies (pretrained) speech activity detection and sends the results for
-    manual correction chunk by chunk.
+    Applies pretrained pipeline and sends the results for manual correction
 
     Parameters
     ----------
-    pipeline :
-        Pretrained speech activity detection pipeline.
+    pipeline : Pipeline
+        Pretrained pipeline.
     source : Path
         Directory containing audio files to process.
+    labels : list of string
+        List of expected pipeline labels.
     chunk : float, optional
-        Duration of chunks, in seconds. Defaults to 10s.
+        Duration of chunks, in seconds. Defaults to 30s.
 
     Yields
     ------
     task : dict
         Prodigy task with the following keys:
         "path" : path to audio file
-        "text" : name of audio file
         "chunk" : chunk start and end times
         "audio" : base64 encoding of audio chunk
-        "audio_spans" : speech spans detected by pretrained SAD model
-        "audio_spans_original" : copy of "audio_spans"
-        "meta" : additional meta-data displayed in Prodigy UI
+        "text" : chunk identifier "{filename} [{start} {end}]"
+        "audio_spans" : list of audio spans {"start": ..., "end": ..., "label": ...}
+        "audio_spans_original" : deep copy of "audio_spans"
+        "meta" : metadata displayed in Prodigy UI {"file": ..., "start": ..., "end": ...}
+        "config": {"labels": list of labels}
     """
 
-    if isinstance(pipeline.classes(), Iterator):
-        extend = 2.5
-    else:
-        extend = 0.5 * pipeline.segmentation_inference_.duration
+    context = getattr(pipeline, "context", 2.5)
 
-    raw_audio = Audio(sample_rate=SAMPLE_RATE, mono=True)
+    audio_for_prodigy = AudioForProdigy()
+    audio_for_pipeline = Audio(mono=True)
 
-    for audio_source in AudioLoader(source):
+    chunks = get_chunks(source, chunk_duration=chunk)
+    if randomize:
+        chunks = list(chunks)
+        random.shuffle(chunks)
 
-        path = audio_source["path"]
-        text = audio_source["text"]
-        file = {"uri": text, "database": source, "audio": path}
+    for file, excerpt in chunks:
 
-        duration = raw_audio.get_duration(file)
-        file["duration"] = duration
+        path = file["path"]
+        filename = file["text"]
+        text = f"{filename} [{excerpt.start:.1f} - {excerpt.end:.1f}]"
 
-        if duration <= chunk:
-            if pipeline is not None:
-                speech: Annotation = pipeline(file)
-                task_audio_spans = to_audio_spans(speech)
-                if isinstance(pipeline.classes(), Iterator):
-                    labels += speech.labels()
-                    labels = list(dict.fromkeys(labels))
-            else:
-                task_audio_spans = []
-            waveform, sr = raw_audio.crop(file, Segment(0, duration))
-            waveform = waveform.numpy().T
-            task_audio = to_base64(normalize(waveform), sample_rate=SAMPLE_RATE)
+        # load contextualized audio excerpt
+        excerpt_with_context = Segment(
+            start=excerpt.start - context, end=excerpt.end + context
+        )
+        waveform_with_context, sample_rate = audio_for_pipeline.crop(
+            path, excerpt_with_context, mode="pad"
+        )
 
-            new_labels = labels.copy()
-            yield {
-                "path": path,
-                "text": text,
-                "audio": task_audio,
-                "audio_spans": task_audio_spans,
-                "audio_spans_original": deepcopy(task_audio_spans),
-                "chunk": {"start": 0, "end": duration},
-                "config": {"labels": new_labels},
-                "meta": {"file": text},
-            }
+        # run pipeline on contextualized audio excerpt
+        output: Annotation = pipeline(
+            {"waveform": waveform_with_context, "sample_rate": sample_rate}
+        )
 
-        else:
-            for focus in chunks(duration, chunk=chunk, shuffle=False):
-                task_text = f"{text} [{focus.start:.1f}, {focus.end:.1f}]"
-                waveform, sr = raw_audio.crop(file, focus)
-                if waveform.shape[1] != SAMPLE_RATE * chunk:
-                    waveform = F.pad(
-                        input=waveform,
-                        pad=(0, int(SAMPLE_RATE * chunk - waveform.shape[1])),
-                        mode="constant",
-                        value=0,
-                    )
-                if pipeline is not None:
-                    longFocus = Segment(
-                        max(0, focus.start - extend), min(focus.end + extend, duration)
-                    )
-                    longWaveform, sr = raw_audio.crop(file, longFocus)
-                    speech: Annotation = pipeline(
-                        {"waveform": longWaveform, "sample_rate": sr}
-                    )
-                    diffStart = focus.start - longFocus.start
-                    trueFocus = Segment(diffStart, diffStart + focus.duration)
-                    speech = speech.crop(trueFocus, mode="intersection")
-                    task_audio_spans = to_audio_spans(speech, focus=trueFocus)
+        # crop, shift, and format output for visualization in Prodigy
+        audio_spans = get_audio_spans(
+            output, excerpt, excerpt_with_context=excerpt_with_context
+        )
 
-                    if isinstance(pipeline.classes(), Iterator):
-                        labels += speech.labels()
-                        labels = list(dict.fromkeys(labels))
-                else:
-                    task_audio_spans = []
+        # load audio excerpt for visualization in Prodigy
+        audio = audio_for_prodigy.crop(path, excerpt)
 
-                waveform = waveform.numpy().T
-                task_audio = to_base64(normalize(waveform), sample_rate=SAMPLE_RATE)
-
-                new_labels = labels.copy()
-                yield {
-                    "path": path,
-                    "text": task_text,
-                    "audio": task_audio,
-                    "audio_spans": task_audio_spans,
-                    "audio_spans_original": deepcopy(task_audio_spans),
-                    "chunk": {"start": focus.start, "end": focus.end},
-                    "config": {"labels": new_labels},
-                    "meta": {
-                        "file": text,
-                        "start": f"{focus.start:.1f}",
-                        "end": f"{focus.end:.1f}",
-                    },
-                }
+        yield {
+            "path": path,
+            "text": text,
+            "audio": audio,
+            "audio_spans": audio_spans,
+            "audio_spans_original": deepcopy(audio_spans),
+            "chunk": {"start": excerpt.start, "end": excerpt.end},
+            "config": {"labels": sorted(set(output.labels()) | set(labels))},
+            "meta": {
+                "file": filename,
+                "start": f"{excerpt.start:.1f}",
+                "end": f"{excerpt.end:.1f}",
+            },
+        }
 
 
 @prodigy.recipe(
@@ -187,8 +156,8 @@ def general_stream(
         None,
         float,
     ),
-    maxspeaker=(
-        "Maximum number of speaker, for speaker diarization",
+    num_classes=(
+        "Set maximum number of classes for pipelines whose number of classes is not predefined (e.g. pyannote/speaker-diarization)",
         "option",
         None,
         int,
@@ -201,42 +170,43 @@ def pipeline(
     source: Union[str, Iterable[dict]],
     pipeline: Union[str, Iterable[dict]],
     chunk: float = 10.0,
-    maxspeaker: int = 4,
+    num_classes: int = 4,
     precision: int = 200,
     beep: bool = False,
 ) -> Dict[str, Any]:
 
-    pipe = Pipeline.from_pretrained(pipeline)
-    classes = pipe.classes()
+    pipeline = Pipeline.from_pretrained(pipeline)
+    classes = pipeline.classes()
+
     if isinstance(classes, Iterator):
-        labels = [x for _, x in zip(range(maxspeaker), classes)]
+        labels = [x for _, x in zip(range(num_classes), classes)]
     else:
         labels = classes
 
-    dirname = os.path.dirname(os.path.realpath(__file__))
-
-    controller_js = dirname + "/../common/controller.js"
+    recipe_dir = Path(__file__).resolve().parent
+    common_dir = recipe_dir.parent / "common"
+    controller_js = common_dir / "controller.js"
     with open(controller_js) as txt:
         javascript = txt.read()
 
-    template = dirname + "/../common/instructions.html"
-    png = dirname + "/../common/commands.png"
-    html = dirname + "/../common/help.html"
-
-    with open(html, "w") as fp, open(template, "r") as fp_tpl, open(
-        png, "rb"
-    ) as fp_png:
+    # TODO: improve this part
+    template = common_dir / "instructions.html"
+    png = common_dir / "commands.png"
+    _, instructions_html = mkstemp(text=True)
+    with open(instructions_html, "w") as instructions_f, open(
+        template, "r"
+    ) as fp_tpl, open(png, "rb") as fp_png:
         b64 = base64.b64encode(fp_png.read()).decode("utf-8")
-        fp.write(fp_tpl.read().replace("{IMAGE}", b64))
+        instructions_f.write(fp_tpl.read().replace("{IMAGE}", b64))
 
     return {
         "view_id": "audio_manual",
         "dataset": dataset,
-        "stream": general_stream(pipe, source, labels, chunk=chunk),
-        "before_db": remove_audio_before_db,
+        "stream": stream(pipeline, source, labels, chunk=chunk, randomize=False),
+        "before_db": before_db,
         "config": {
             "javascript": javascript,
-            "instructions": html,
+            "instructions": instructions_html,
             "buttons": ["accept", "ignore", "undo"],
             "keymap": {
                 "accept": ["enter"],
