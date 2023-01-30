bfb3760524d5c8c793e766ddd6e245f555fb89d4
Hervé BREDIN <hbredin@users.noreply.github.com>
2023-01-30 10:28:47 +0100

    fix: fix embedding validation

--- pyannote/audio/tasks/embedding/mixins.py
+++ pyannote/audio/tasks/embedding/mixins.py
@@ -218,28 +218,19 @@ class SupervisedRepresentationLearningTaskMixin:
         avg_chunk_duration = 0.5 * (self.min_duration + self.duration)
         return max(self.batch_size, math.ceil(duration / avg_chunk_duration))
 
-    def collate_X(self, batch) -> torch.Tensor:
-        return default_collate([b["X"] for b in batch])
-
-    def collate_y(self, batch) -> torch.Tensor:
-        return default_collate([b["y"] for b in batch])
-
     def collate_fn(self, batch, stage="train"):
 
-        # collate X
-        collated_X = self.collate_X(batch)
+        collated = default_collate(batch)
 
-        # collate y
-        collated_y = self.collate_y(batch)
+        if stage == "train":
+            self.augmentation.train(mode=True)
+            augmented = self.augmentation(
+                samples=collated["X"],
+                sample_rate=self.model.hparams.sample_rate,
+            )
+            collated["X"] = augmented.samples
 
-        # apply augmentation (only in "train" stage)
-        self.augmentation.train(mode=(stage == "train"))
-        augmented = self.augmentation(
-            samples=collated_X,
-            sample_rate=self.model.hparams.sample_rate,
-        )
-
-        return {"X": augmented.samples, "y": collated_y}
+        return collated
 
     def training_step(self, batch, batch_idx: int):
 
@@ -257,6 +248,7 @@ class SupervisedRepresentationLearningTaskMixin:
         return {"loss": loss}
 
     def val__getitem__(self, idx):
+
         if isinstance(self.protocol, SpeakerVerificationProtocol):
             trial = self._validation[idx]
 
@@ -312,6 +304,3 @@ class SupervisedRepresentationLearningTaskMixin:
                 prog_bar=True,
                 logger=True,
             )
-
-        elif isinstance(self.protocol, SpeakerDiarizationProtocol):
-            pass
