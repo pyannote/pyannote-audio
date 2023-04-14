1d8b47c13a68bab1de53bd9c052369e2575a7bba
Hervé BREDIN <hbredin@users.noreply.github.com>
2023-04-14 12:48:54 +0200

    chore: remove temp file

--- temp/pytorch20.py
+++
@@ -1,9 +0,0 @@
-from pyannote.audio import Pipeline
-from pyannote.audio.pipelines.utils.hook import ProgressHook
-
-pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization")
-pipeline.embedding_batch_size = 1
-pipeline.to("mps")
-with ProgressHook() as hook:
-    diarization = pipeline("../tests/data/trn00.wav", hook=hook)
-print(diarization)
