184e58c6b653545d9148d6771677eac717958a07
clement-pages <clement.pages@irit.fr>
2023-12-22 12:17:17 +0100

    update: make `cache` a pytest fixture in training tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -14,14 +14,17 @@ from pyannote.audio.tasks import (
     VoiceActivityDetection,
 )
 
-CACHE_FILE_PATH = "./cache/cache_file"
-
 
 @pytest.fixture()
 def protocol():
     return get_protocol(
         "Debug.SpeakerDiarization.Debug", preprocessors={"audio": FileFinder()}
     )
+
+
+@pytest.fixture()
+def cache():
+    return "./cache/cache_file"
 
 
 @pytest.fixture()
@@ -51,21 +54,19 @@ def test_train_segmentation(protocol):
     trainer.fit(model)
 
 
-def test_train_segmentation_with_cached_data_mono_device(protocol):
+def test_train_segmentation_with_cached_data_mono_device(protocol, cache):
     try:
-        first_task = SpeakerDiarization(protocol, cache=CACHE_FILE_PATH)
+        first_task = SpeakerDiarization(protocol, cache=cache)
         first_model = SimpleSegmentationModel(task=first_task)
         first_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         first_trainer.fit(first_model)
 
-        second_task = SpeakerDiarization(protocol, cache=CACHE_FILE_PATH)
+        second_task = SpeakerDiarization(protocol, cache=cache)
         second_model = SimpleSegmentationModel(task=second_task)
         second_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         second_trainer.fit(second_model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_train_multilabel_segmentation(gender_protocol):
@@ -75,21 +76,21 @@ def test_train_multilabel_segmentation(gender_protocol):
     trainer.fit(model)
 
 
-def test_train_multilabel_segmentation_with_cached_data_mono_device(gender_protocol):
+def test_train_multilabel_segmentation_with_cached_data_mono_device(
+    gender_protocol, cache
+):
     try:
-        first_task = MultiLabelSegmentation(gender_protocol, cache=CACHE_FILE_PATH)
+        first_task = MultiLabelSegmentation(gender_protocol, cache=cache)
         first_model = SimpleSegmentationModel(task=first_task)
         first_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         first_trainer.fit(first_model)
 
-        second_task = MultiLabelSegmentation(gender_protocol, cache=CACHE_FILE_PATH)
+        second_task = MultiLabelSegmentation(gender_protocol, cache=cache)
         second_model = SimpleSegmentationModel(task=second_task)
         second_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         second_trainer.fit(second_model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_train_supervised_representation_with_arcface(protocol):
@@ -108,21 +109,19 @@ def test_train_voice_activity_detection(protocol):
     trainer.fit(model)
 
 
-def test_train_voice_activity_detection_with_cached_data_mono_device(protocol):
+def test_train_voice_activity_detection_with_cached_data_mono_device(protocol, cache):
     try:
-        first_task = VoiceActivityDetection(protocol, cache=CACHE_FILE_PATH)
+        first_task = VoiceActivityDetection(protocol, cache=cache)
         first_model = SimpleSegmentationModel(task=first_task)
         first_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         first_trainer.fit(first_model)
 
-        second_task = VoiceActivityDetection(protocol, cache=CACHE_FILE_PATH)
+        second_task = VoiceActivityDetection(protocol, cache=cache)
         second_model = SimpleSegmentationModel(task=second_task)
         second_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         second_trainer.fit(second_model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_train_overlapped_speech_detection(protocol):
@@ -132,21 +131,21 @@ def test_train_overlapped_speech_detection(protocol):
     trainer.fit(model)
 
 
-def test_train_overlapped_speech_detection_with_cached_data_mono_device(protocol):
+def test_train_overlapped_speech_detection_with_cached_data_mono_device(
+    protocol, cache
+):
     try:
-        first_task = OverlappedSpeechDetection(protocol, cache=CACHE_FILE_PATH)
+        first_task = OverlappedSpeechDetection(protocol, cache=cache)
         first_model = SimpleSegmentationModel(task=first_task)
         first_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         first_trainer.fit(first_model)
 
-        second_task = OverlappedSpeechDetection(protocol, cache=CACHE_FILE_PATH)
+        second_task = OverlappedSpeechDetection(protocol, cache=cache)
         second_model = SimpleSegmentationModel(task=second_task)
         second_trainer = Trainer(fast_dev_run=True, accelerator="cpu", devices=1)
         second_trainer.fit(second_model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_finetune_with_task_that_does_not_need_setup_for_specs(protocol):
@@ -173,21 +172,19 @@ def test_finetune_with_task_that_needs_setup_for_specs(protocol):
     trainer.fit(model)
 
 
-def test_finetune_with_task_that_needs_setup_for_specs_and_with_cache(protocol):
+def test_finetune_with_task_that_needs_setup_for_specs_and_with_cache(protocol, cache):
     try:
-        segmentation = SpeakerDiarization(protocol, cache=CACHE_FILE_PATH)
+        segmentation = SpeakerDiarization(protocol, cache=cache)
         model = SimpleSegmentationModel(task=segmentation)
         trainer = Trainer(fast_dev_run=True, accelerator="cpu")
         trainer.fit(model)
 
-        segmentation = SpeakerDiarization(protocol, cache=CACHE_FILE_PATH)
+        segmentation = SpeakerDiarization(protocol, cache=cache)
         model.task = segmentation
         trainer = Trainer(fast_dev_run=True, accelerator="cpu")
         trainer.fit(model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_transfer_with_task_that_does_not_need_setup_for_specs(protocol):
@@ -227,22 +224,22 @@ def test_finetune_freeze_with_task_that_needs_setup_for_specs(protocol):
     trainer.fit(model)
 
 
-def test_finetune_freeze_with_task_that_needs_setup_for_specs_and_with_cache(protocol):
+def test_finetune_freeze_with_task_that_needs_setup_for_specs_and_with_cache(
+    protocol, cache
+):
     try:
-        segmentation = SpeakerDiarization(protocol, cache=CACHE_FILE_PATH)
+        segmentation = SpeakerDiarization(protocol, cache=cache)
         model = SimpleSegmentationModel(task=segmentation)
         trainer = Trainer(fast_dev_run=True, accelerator="cpu")
         trainer.fit(model)
 
-        segmentation = SpeakerDiarization(protocol, cache=CACHE_FILE_PATH)
+        segmentation = SpeakerDiarization(protocol, cache=cache)
         model.task = segmentation
         model.freeze_by_name("mfcc")
         trainer = Trainer(fast_dev_run=True, accelerator="cpu")
         trainer.fit(model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_finetune_freeze_with_task_that_does_not_need_setup_for_specs(protocol):
@@ -260,22 +257,21 @@ def test_finetune_freeze_with_task_that_does_not_need_setup_for_specs(protocol):
 
 def test_finetune_freeze_with_task_that_does_not_need_setup_for_specs_and_with_cache(
     protocol,
+    cache,
 ):
     try:
-        vad = VoiceActivityDetection(protocol, cache=CACHE_FILE_PATH)
+        vad = VoiceActivityDetection(protocol, cache=cache)
         model = SimpleSegmentationModel(task=vad)
         trainer = Trainer(fast_dev_run=True, accelerator="cpu")
         trainer.fit(model)
 
-        vad = VoiceActivityDetection(protocol, cache=CACHE_FILE_PATH)
+        vad = VoiceActivityDetection(protocol, cache=cache)
         model.task = vad
         model.freeze_by_name("mfcc")
         trainer = Trainer(fast_dev_run=True, accelerator="cpu")
         trainer.fit(model)
-    except:
-        raise
     finally:
-        Path(CACHE_FILE_PATH).unlink(missing_ok=True)
+        Path(cache).unlink(missing_ok=True)
 
 
 def test_transfer_freeze_with_task_that_does_not_need_setup_for_specs(protocol):
