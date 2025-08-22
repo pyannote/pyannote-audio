c02f08cfafc7f08d5ed078c2e5e886d4088e5abb
clement-pages <clement.pages@etik.com>
2025-08-22 15:31:38 +0200

    add missing benchmark name in path

--- src/pyannote/audio/__main__.py
+++ src/pyannote/audio/__main__.py
@@ -619,7 +619,7 @@ def benchmark(
     speaker_count: dict[int, dict[int, int]] = dict()
 
     if per_file:
-        rttm_dir = into / "rttms"
+        rttm_dir = into / benchmark_name / "rttm"
         rttm_dir.mkdir()
 
     # iterate over all files in the specified subset
@@ -639,7 +639,7 @@ def benchmark(
         # if prediction has a built-in serialize method, save serialized version
         if hasattr(prediction, "serialize"):
             if per_file:
-                json_dir = into / "jsons"
+                json_dir = into / benchmark_name / "json"
                 json_dir.mkdir(exist_ok=True)
 
                 with open(json_dir / f"{uri}.json", "w") as f:
