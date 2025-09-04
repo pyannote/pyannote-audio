adea13a1cfe36da8ba0f11ece9e0ed7ef85f4390
Clément Pagés <clement.pages@etik.com>
2025-09-04 15:20:33 +0200

    Apply suggestion from @hbredin
    Co-authored-by: Hervé BREDIN <hbredin@users.noreply.github.com>

--- src/pyannote/audio/__main__.py
+++ src/pyannote/audio/__main__.py
@@ -800,7 +800,7 @@ def benchmark(
                 raise FileExistsError(f"{optimized_rttm_file} already exists.")
         for file in files:
             if per_file:
-                optimized_rttm_file = optimized_rttm_dir / f"{file['uri']}_OptimizedMinDurationOff.rttm"
+                optimized_rttm_file = optimized_rttm_dir / f"{file['uri']}.OptimizedMinDurationOff.rttm"
   
             with open(optimized_rttm_file, "w" if per_file else "a") as rttm:
                 file["best_speaker_diarization"].write_rttm(rttm)
