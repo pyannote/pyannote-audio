2c25e37c41d12425a96bdd95a29bd3f3c74e2d6f
Hervé BREDIN <hbredin@users.noreply.github.com>
2025-12-15 13:55:20 +0100

    checking if I can push to clement's fork

--- src/pyannote/audio/__main__.py
+++ src/pyannote/audio/__main__.py
@@ -4,7 +4,7 @@
 # MIT License
 #
 # Copyright (c) 2024-2025 CNRS
-# Copyright (c) 2025 pyannoteAI
+# Copyright (c) 2025- pyannoteAI
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
